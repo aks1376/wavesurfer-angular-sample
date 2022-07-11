e84f56de98377797c6cbaddc6aedc4a69ca19eca
Ali Karimi <alikarimi.s1376@gmail.com>
2022-07-11 09:47:38 +0430

    added load local file added create audio url from file added audio url to wavesurfer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WavesurferAngularSample';
+  audioUrl!: string;
+
+  onAddAudioFile(event: Event) {
+    if (event) {
+      const files = (event.target as HTMLInputElement).files;
+      if (files && files.length !== 0) {
+        this.audioUrl = URL.createObjectURL(files[0]);
+      }
+    }
+  }
 }
--- src/app/wavesurfer/wavesurfer.component.ts
+++ src/app/wavesurfer/wavesurfer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import WaveSurfer from 'wavesurfer.js';
 
 @Component({
@@ -8,10 +8,46 @@ import WaveSurfer from 'wavesurfer.js';
 })
 export class WavesurferComponent implements OnInit {
 
+  @Input('audioUrl') set setAudioUrl(audioUrl: string) {
+    if (audioUrl) {
+      this.loadAudioUrl(audioUrl);
+    }
+  }
+
+  wavesurfer!: WaveSurfer;
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.createWaveSurfer();
+    this.playWhenReady();
   }
+
+  createWaveSurfer() {
+    this.wavesurfer = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: 'violet',
+      progressColor: 'purple'
+    });
+  }
+
+  playWhenReady() {
+    this.wavesurfer.on('ready', () => {
+      this.wavesurfer.play();
+    });
+    this.wavesurfer.on('audioprocess', (event: any) => {
+      console.log(event);
+    });
+  }
+
+  loadAudioUrl(url: string) {
+    this.wavesurfer.load(url);
+  }
+
+  onPlayPause() {
+    this.wavesurfer.playPause();
+  }
+
 
 }
