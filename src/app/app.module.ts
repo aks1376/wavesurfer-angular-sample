a0fcda59e2a11260cea83df9298445f7b968929b
Ali Karimi <alikarimi.s1376@gmail.com>
2022-07-11 09:22:59 +0430

    installed wavesurfer library enabled allowSyntheticDefaultImports flag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WavesurferComponent } from './wavesurfer/wavesurfer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WavesurferComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/wavesurfer/wavesurfer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import WaveSurfer from 'wavesurfer.js';
+
+@Component({
+  selector: 'app-wavesurfer',
+  templateUrl: './wavesurfer.component.html',
+  styleUrls: ['./wavesurfer.component.scss']
+})
+export class WavesurferComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
